a2bbf7ef48706e8ef1907f98ab586853f28ae4aa
bencelaszlo <bencelaszlo@protonmail.com>
2019-03-19 17:29:49 +0100

    Impement file_read_int and abs_float functions

--- include/utils.h
+++ include/utils.h
@@ -1,4 +1,10 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
+
     Vec file_read(char file_path[]);
+
+    Vec_int file_read_int(char file_path[]);
+
+    float abs_float(float number);
+
 #endif
--- src/utils.c
+++ src/utils.c
@@ -24,3 +24,35 @@ Vec file_read(char file_path[]) {
 
     return input_numbers;
 }
+
+Vec_int file_read_int(char file_path[]) {
+    Vec_int input_numbers;
+    vec_init_int(&input_numbers, 1);
+
+    FILE *fp;
+
+    fp = fopen(file_path, "r");
+    if (fp == NULL) {
+        printf("File read error.");
+    }
+
+    int temp_number;
+    while(!feof(fp) ) {
+        fscanf(fp, "%d", &temp_number);
+        vec_insert_int(&input_numbers, temp_number);
+    }
+
+    fclose(fp);
+
+    return input_numbers;
+}
+
+float abs_float(float number) {
+    float abs_value = number;
+
+    if (number < 0.0f) {
+        abs_value *= -1.0f;
+    }
+
+    return abs_value;
+}
